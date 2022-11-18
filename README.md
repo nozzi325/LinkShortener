946b202180de25c6d3eee8746a87c1334d861949
nozzi325 <nozzia3@gmail.com>
2022-11-19 00:25:03 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# LinkShortener App
+// TODO
---
+++ src/test/java/by/zhukovsky/LinkShortener/LinkShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.zhukovsky.LinkShortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
