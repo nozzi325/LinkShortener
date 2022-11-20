706f51831cbb9400bf1684fd486707ee6dfd9c3b
nozzi325 <nozzia3@gmail.com>
2022-11-20 16:50:59 +0300

    Added @AllArgsConstructor and @NoArgsConstructor annotations

--- src/main/java/by/zhukovsky/LinkShortener/dto/ErrorResponse.java
+++ src/main/java/by/zhukovsky/LinkShortener/dto/ErrorResponse.java
@@ -1,8 +1,12 @@
 package by.zhukovsky.LinkShortener.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ErrorResponse {
     private String message;
 }
