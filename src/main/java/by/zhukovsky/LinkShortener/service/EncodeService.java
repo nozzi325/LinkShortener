ab00497d6bd96dd9f2e2056d8abd0f026a407347
nozzi325 <nozzia3@gmail.com>
2023-10-10 15:27:46 +0300

    Refactored EncodeService

--- src/main/java/by/zhukovsky/LinkShortener/service/EncodeService.java
+++ src/main/java/by/zhukovsky/LinkShortener/service/EncodeService.java
@@ -9,14 +9,13 @@ public class EncodeService {
     private static final int LENGTH = 7;
     private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     private Random random = new Random();
+
     public String generateRandomUrl() {
         char[] result = new char[LENGTH];
-        while (true) {
-            for (int i = 0; i < LENGTH; i++) {
-                int randomIndex = random.nextInt(CHARACTERS.length() - 1);
-                result[i] = CHARACTERS.charAt(randomIndex);
-            }
-            return new String(result);
+        for (int i = 0; i < LENGTH; i++) {
+            int randomIndex = random.nextInt(CHARACTERS.length() - 1);
+            result[i] = CHARACTERS.charAt(randomIndex);
         }
+        return new String(result);
     }
 }
