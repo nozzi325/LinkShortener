2dfc7c8703c5de7be077944b8fa282b8d140f26c
nozzi325 <nozzia3@gmail.com>
2023-10-10 16:12:57 +0300

    Updated code formatting, removed 'var' usage

--- src/main/java/by/zhukovsky/LinkShortener/service/LinkService.java
+++ src/main/java/by/zhukovsky/LinkShortener/service/LinkService.java
@@ -11,7 +11,8 @@ import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityExistsException;
 import javax.persistence.EntityNotFoundException;
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LinkService {
@@ -26,48 +27,50 @@ public class LinkService {
         this.statsService = statsService;
     }
 
-    public String createShortLink(LinkRequest request){
+    public String createShortLink(LinkRequest request) {
         String originalUrl = request.getOriginal();
 
-        if (repository.existsByOriginalLink(originalUrl)){
-            throw new EntityExistsException(String.format("Link for %s already exists", originalUrl));
+        if (repository.existsByOriginalLink(originalUrl)) {
+            throw new EntityExistsException(String.format("Short link for %s already exists", originalUrl));
         }
 
         String shortUrl = encoder.generateRandomUrl();
 
-        var entity = new Link()
+        Link createdLink = new Link()
                 .setOriginalLink(originalUrl)
                 .setShortLink(shortUrl);
 
         statsService.addToStatsMap(shortUrl);
-        repository.save(entity);
+        repository.save(createdLink);
 
         return shortUrl;
     }
 
     public String getOriginalUrl(String shortUrl) {
-        var entity = repository.findByShortLink(shortUrl)
-                .orElseThrow(() -> new EntityNotFoundException(String.format("Link for \"%s\" not found",shortUrl)));
+        Link entity = repository.findByShortLink(shortUrl)
+                .orElseThrow(
+                        () -> new EntityNotFoundException(String.format("Requested resource '%s' not found", shortUrl))
+                );
         statsService.incrementLinkUsageCounter(shortUrl);
         return entity.getOriginalLink();
     }
 
-    public StatsResponse getLinkStats(String shortUrl){
-        var entity =  statsService.getTotalStats().stream()
+    public StatsResponse getLinkStats(String shortUrl) {
+        Optional<StatsResponse> entity =  statsService.getTotalStats().stream()
                 .filter(link -> link.getLink().contains(shortUrl))
                 .findFirst();
 
         if (!entity.isPresent()){
-            throw new EntityNotFoundException(String.format("Link for \"%s\" not found",shortUrl));
+            throw new EntityNotFoundException(String.format("Link for '%s' not found", shortUrl));
         }
 
         return entity.get();
     }
 
-    public List<StatsResponse> getPagedStats(Pageable pageable){
-        var totalStats = statsService.getTotalStats();
+    public List<StatsResponse> getPagedStats(Pageable pageable) {
+        List<StatsResponse> totalStats = statsService.getTotalStats();
 
-        int start = Math.min((int) pageable.getOffset(),totalStats.size());
+        int start = Math.min((int) pageable.getOffset(), totalStats.size());
         int end = Math.min(start + pageable.getPageSize(), totalStats.size());
 
         Page<StatsResponse> page = new PageImpl<>(
