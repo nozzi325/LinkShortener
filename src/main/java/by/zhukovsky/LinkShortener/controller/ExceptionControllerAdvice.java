2907348ce6f72b2984dbc36ee501ea2aeadc14ac
nozzi325 <nozzia3@gmail.com>
2023-10-10 15:27:28 +0300

    Refactored ExceptionControllerAdvice

--- src/main/java/by/zhukovsky/LinkShortener/controller/ExceptionControllerAdvice.java
+++ src/main/java/by/zhukovsky/LinkShortener/controller/ExceptionControllerAdvice.java
@@ -3,6 +3,7 @@ package by.zhukovsky.LinkShortener.controller;
 import by.zhukovsky.LinkShortener.dto.ErrorResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -11,41 +12,43 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.persistence.EntityExistsException;
 import javax.persistence.EntityNotFoundException;
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class ExceptionControllerAdvice {
-    Logger logger = LoggerFactory.getLogger(LinkController.class);
+    Logger logger = LoggerFactory.getLogger(ExceptionControllerAdvice.class);
 
     @ExceptionHandler(EntityExistsException.class)
     public ResponseEntity<ErrorResponse> exceptionEntityExistsHandler(EntityExistsException e) {
         logger.error(e.getMessage());
-        ErrorResponse error = new ErrorResponse();
-        error.setMessage(e.getMessage());
+        ErrorResponse error = createErrorResponse(e.getMessage());
         return ResponseEntity
-                .badRequest()
+                .status(HttpStatus.BAD_REQUEST)
                 .body(error);
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ErrorResponse> exceptionEntityNotFoundHandler(EntityNotFoundException e) {
         logger.error(e.getMessage());
-        ErrorResponse error = new ErrorResponse();
-        error.setMessage(e.getMessage());
+        ErrorResponse error = createErrorResponse(e.getMessage());
         return ResponseEntity
-                .notFound()
-                .build();
+                .status(HttpStatus.NOT_FOUND)
+                .body(error);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> exceptionMethodArgumentNotValidHandler(MethodArgumentNotValidException e) {
-        ErrorResponse errorDetails = new ErrorResponse();
         StringBuilder errMessage = new StringBuilder();
         for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
-            errMessage.append(String.format("%s; ",fieldError.getDefaultMessage()));
+            errMessage.append(String.format("%s; ", fieldError.getDefaultMessage()));
         }
-        errorDetails.setMessage(String.valueOf(errMessage).trim());
+        ErrorResponse errorDetails = createErrorResponse(String.valueOf(errMessage).trim());
         return ResponseEntity
-                .badRequest()
+                .status(HttpStatus.BAD_REQUEST)
                 .body(errorDetails);
+    }
+
+    private ErrorResponse createErrorResponse(String message) {
+        return new ErrorResponse(message, LocalDateTime.now());
     }
 }
