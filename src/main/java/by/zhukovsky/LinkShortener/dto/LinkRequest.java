c6ebacb8ce195a5df76f3bd6533be9df5a5a409d
nozzi325 <nozzia3@gmail.com>
2023-10-11 20:38:41 +0300

    Removed @NotNull annotation

--- src/main/java/by/zhukovsky/LinkShortener/dto/LinkRequest.java
+++ src/main/java/by/zhukovsky/LinkShortener/dto/LinkRequest.java
@@ -1,11 +1,9 @@
 package by.zhukovsky.LinkShortener.dto;
 
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 public record LinkRequest(
         @NotEmpty(message = "Link field can't be empty")
-        @NotNull(message = "Request can't be blank")
         String original
 ) {
 }
