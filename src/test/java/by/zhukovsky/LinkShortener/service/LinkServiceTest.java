7747e1d9020f0d8caf7c9ee882dddb2206bcf018
nozzi325 <nozzia3@gmail.com>
2023-10-11 22:31:56 +0300

    Rewrite tests according to new logic

--- src/test/java/by/zhukovsky/LinkShortener/service/LinkServiceTest.java
+++ src/test/java/by/zhukovsky/LinkShortener/service/LinkServiceTest.java
@@ -4,213 +4,142 @@ import by.zhukovsky.LinkShortener.dto.LinkRequest;
 import by.zhukovsky.LinkShortener.dto.StatsResponse;
 import by.zhukovsky.LinkShortener.entity.Link;
 import by.zhukovsky.LinkShortener.repository.LinkRepository;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import javax.persistence.EntityExistsException;
 import javax.persistence.EntityNotFoundException;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
 class LinkServiceTest {
-    @InjectMocks LinkService subj;
-    @Mock LinkRepository linkRepository;
-    @Mock EncodeService encoder;
-    @Mock StatsService statsService;
+    @Mock
+    private EncodeService encodeService;
 
+    @Mock
+    private LinkRepository linkRepository;
+
+    private LinkService linkService;
 
     @BeforeEach
     void setUp() {
-        Map<String, Integer> linkCounterMap = new HashMap<>();
-        statsService.setLinkCounterMap(linkCounterMap);
-    }
-
-    @AfterEach
-    void tearDown(){
-        statsService.setLinkCounterMap(Collections.emptyMap());
+        MockitoAnnotations.openMocks(this);
+        linkService = new LinkService(encodeService, linkRepository);
     }
 
     @Test
-    @DisplayName("Should create short link and return it")
-    void shouldCreateShortLink() {
-        //given
-        String originalUrl = "original_link";
-        String shortUrl = "short_link";
-        LinkRequest linkRequest = new LinkRequest(originalUrl);
-        Link entity = new Link(originalUrl, shortUrl);
+    void createShortLink_ShouldGenerateShortLink() {
+        LinkRequest linkRequest = new LinkRequest("https://example.com");
+        when(linkRepository.existsByOriginalLink(linkRequest.original())).thenReturn(false);
+        when(encodeService.generateRandomUrl()).thenReturn("abc123");
 
-        when(linkRepository.existsByOriginalLink(originalUrl)).thenReturn(false);
-        when(encoder.generateRandomUrl()).thenReturn(shortUrl);
+        String shortLink = linkService.createShortLink(linkRequest);
 
-        //when
-        String actualValue = subj.createShortLink(linkRequest);
-
-        //then
-        verify(statsService).addToStatsMap(shortUrl);
-        verify(linkRepository).save(entity);
-        assertEquals(shortUrl, actualValue);
+        assertEquals("/l/abc123", shortLink);
     }
 
     @Test
-    @DisplayName("Should throw EntityExistsException when a short link " +
-            "for the provided link already exists")
-    void shouldThrowEntityExistsException_WhenShortcutForLinkAlreadyExists() {
-        //given
-        String originalUrl = "original_link";
-        LinkRequest linkRequest = new LinkRequest(originalUrl);
-        when(linkRepository.existsByOriginalLink(originalUrl)).thenReturn(true);
+    void createShortLink_ShouldThrowEntityExistsException() {
+        LinkRequest linkRequest = new LinkRequest("https://example.com");
+        when(linkRepository.existsByOriginalLink(linkRequest.original())).thenReturn(true);
 
-        //when & then
-        assertThrows(EntityExistsException.class, () -> subj.createShortLink(linkRequest));
-        verify(statsService, never()).addToStatsMap(any());
-        verify(linkRepository, never()).save(any());
-        assertTrue(statsService.getLinkCounterMap().isEmpty());
+        EntityExistsException exception = assertThrows(EntityExistsException.class,
+                () -> linkService.createShortLink(linkRequest));
+        assertEquals("Short link for 'https://example.com' already exists", exception.getMessage());
     }
 
     @Test
-    @DisplayName("Should return original link and increment usage counter for this link")
-    void shouldReturnOriginalUrlAndIncrementCounter() {
-        //given
-        String originalUrl = "original_link";
-        String shortUrl = "short_link";
-        Link entity = new Link(1L, originalUrl, shortUrl);
-        when(linkRepository.findByShortLink(shortUrl)).thenReturn(Optional.of(entity));
+    void getOriginalUrl_ShouldReturnOriginalUrl() {
+        String shortUrl = "/l/abc123";
+        Link link = new Link("https://example.com", "abc123");
+        when(linkRepository.findByShortLink(shortUrl)).thenReturn(Optional.of(link));
 
-        //when
-        String actualValue = subj.getOriginalUrl(shortUrl);
+        String originalUrl = linkService.getOriginalUrl(shortUrl);
 
-        //then
-        verify(statsService).incrementLinkUsageCounter(shortUrl);
-        assertEquals(originalUrl, actualValue);
+        assertEquals("https://example.com", originalUrl);
+        assertEquals(1, link.getCount());
     }
 
     @Test
-    @DisplayName("Should throw EntityNotFoundException when provided short url doesn't exist")
-    void shouldThrowEntityNotFoundException_WhenShortLinkDoesNotExist() {
-        //given
-        String shortUrl = "short_link";
+    void getOriginalUrl_ShouldThrowEntityNotFoundException() {
+        String shortUrl = "/l/nonexistent";
         when(linkRepository.findByShortLink(shortUrl)).thenReturn(Optional.empty());
 
-        //when & then
-        assertThrows(EntityNotFoundException.class, () -> subj.getOriginalUrl(shortUrl));
+        EntityNotFoundException exception = assertThrows(EntityNotFoundException.class,
+                () -> linkService.getOriginalUrl(shortUrl));
+        assertEquals("Requested resource '/l/nonexistent' not found", exception.getMessage());
     }
 
     @Test
-    @DisplayName("Should return list of top-4 links")
-    void shouldReturnStatsForTop4Links() {
-        //given
-        statsService.getLinkCounterMap().put("short_link1", 5);
-        statsService.getLinkCounterMap().put("short_link2", 3);
-        statsService.getLinkCounterMap().put("short_link3", 4);
-        statsService.getLinkCounterMap().put("short_link4", 1);
-        statsService.getLinkCounterMap().put("short_link5", 0);
-        StatsResponse stats1 = new StatsResponse("short_link1", "original_link1", 1, 5);
-        StatsResponse stats2 = new StatsResponse("short_link2", "original_link2", 3, 3);
-        StatsResponse stats3 = new StatsResponse("short_link3", "original_link3", 2, 4);
-        StatsResponse stats4 = new StatsResponse("short_link4", "original_link4", 4, 1);
-        StatsResponse stats5 = new StatsResponse("short_link5", "original_link5", 5, 0);
-        List<StatsResponse> list = List.of(stats1, stats2, stats3, stats4, stats5);
-        int page = 0;
-        int count = 4;
-        Pageable pageable = PageRequest.of(page, count);
-        when(statsService.getTotalStats()).thenReturn(list);
+    void getLinkStats_OK() {
+        String shortUrl = "/l/abc123";
+        Link link = new Link("https://example.com", "abc123");
+        when(linkRepository.findByShortLink(shortUrl)).thenReturn(Optional.of(link));
+        when(linkRepository.getRankByCount(link.getCount())).thenReturn(5);
 
-        //when
-        List<StatsResponse> results = subj.getPagedStats(pageable);
+        StatsResponse statsResponse = linkService.getLinkStats(shortUrl);
 
-        //verify
-        assertEquals(count, results.size());
-        assertTrue(results.contains(stats1));
-        assertTrue(results.contains(stats2));
-        assertTrue(results.contains(stats3));
-        assertTrue(results.contains(stats4));
-        assertFalse(results.contains(stats5));
+        assertEquals("/l/abc123", statsResponse.link());
+        assertEquals("https://example.com", statsResponse.original());
+        assertEquals(6, statsResponse.rank());
+        assertEquals(0, statsResponse.count());
     }
 
     @Test
-    @DisplayName("Should return empty list when page number and items count greater" +
-            "than total link count")
-    void shouldReturnEmptyList_WhenPageNumberAndItemCounterGreaterThanTotalLinkCount() {
-        //given
-        statsService.getLinkCounterMap().put("short_link1", 5);
-        statsService.getLinkCounterMap().put("short_link2", 3);
-        statsService.getLinkCounterMap().put("short_link3", 4);
-        statsService.getLinkCounterMap().put("short_link4", 1);
-        statsService.getLinkCounterMap().put("short_link5", 0);
-        StatsResponse stats1 = new StatsResponse("short_link1", "original_link1", 1, 5);
-        StatsResponse stats2 = new StatsResponse("short_link2", "original_link2", 3, 3);
-        StatsResponse stats3 = new StatsResponse("short_link3", "original_link3", 2, 4);
-        StatsResponse stats4 = new StatsResponse("short_link4", "original_link4", 4, 1);
-        StatsResponse stats5 = new StatsResponse("short_link5", "original_link5", 5, 0);
-        List<StatsResponse> list = List.of(stats1, stats2, stats3, stats4, stats5);
-        int page = 99;
-        int count = 100;
-        Pageable pageable = PageRequest.of(page, count);
-        when(statsService.getTotalStats()).thenReturn(list);
+    void getTotalStats_OK() {
+        List<Link> linkList = List.of(
+                new Link("https://example1.com", "abc1"),
+                new Link("https://example2.com", "abc2"),
+                new Link("https://example3.com", "abc3")
+        );
+        Page<Link> mockPage = new PageImpl<>(linkList);
+        when(linkRepository.findAllByOrderByCountDesc(Mockito.any(Pageable.class))).thenReturn(mockPage);
 
-        //when
-        List<StatsResponse> results = subj.getPagedStats(pageable);
+        List<StatsResponse> statsResponses = linkService.getTotalStats(PageRequest.of(0, 10));
 
-        //verify
-        assertTrue(results.isEmpty());
+        List<StatsResponse> expectedStatsResponses = linkList.stream()
+                .map(link -> new StatsResponse(
+                        link.getOriginalLink(),
+                        "/l/" + link.getShortLink(),
+                        linkList.indexOf(link) + 1,
+                        link.getCount())
+                )
+                .collect(Collectors.toList());
+
+        assertEquals(expectedStatsResponses, statsResponses);
     }
 
     @Test
-    @DisplayName("Should return one link stats by it short url")
-    void shouldReturnStatsForOneLink() {
-        //given
-        statsService.getLinkCounterMap().put("short_link1", 5);
-        statsService.getLinkCounterMap().put("short_link2", 3);
-        statsService.getLinkCounterMap().put("short_link3", 4);
-        StatsResponse stats1 = new StatsResponse("short_link1", "original_link1", 1, 5);
-        StatsResponse stats2 = new StatsResponse("short_link2", "original_link2", 3, 3);
-        StatsResponse stats3 = new StatsResponse("short_link3", "original_link3", 2, 4);
-        List<StatsResponse> list = List.of(stats1, stats2, stats3);
-        when(statsService.getTotalStats()).thenReturn(list);
+    void findLinkByShortUrl_ShouldReturnLink() {
+        String shortUrl = "/l/abc123";
+        Link link = new Link("https://example.com", "abc123");
+        when(linkRepository.findByShortLink(shortUrl)).thenReturn(Optional.of(link));
 
-        //when
-        StatsResponse result = subj.getLinkStats("short_link2");
+        Link foundLink = ReflectionTestUtils.invokeMethod(linkService, "findLinkByShortUrl", shortUrl);
 
-        //then
-        assertEquals(stats2, result);
+        assertEquals(link, foundLink);
     }
 
     @Test
-    @DisplayName("Should return EntityNotFoundException" +
-            " when trying to get stats for non-existing short url")
-    void shouldThrowEntityNotFoundExceptionWhenTryingToFindStatsForNonExistingLink() {
-        //given
-        statsService.getLinkCounterMap().put("short_link1", 5);
-        statsService.getLinkCounterMap().put("short_link2", 3);
-        statsService.getLinkCounterMap().put("short_link3", 4);
-        StatsResponse stats1 = new StatsResponse("short_link1", "original_link1", 1, 5);
-        StatsResponse stats2 = new StatsResponse("short_link2", "original_link2", 3, 3);
-        StatsResponse stats3 = new StatsResponse("short_link3", "original_link3", 2, 4);
-        List<StatsResponse> list = List.of(stats1, stats2, stats3);
-        when(statsService.getTotalStats()).thenReturn(list);
+    void findLinkByNonExistentShortUrl_ShouldThrowEntityNotFoundException() {
+        String shortUrl = "/l/nonexistent";
+        when(linkRepository.findByShortLink(shortUrl)).thenReturn(Optional.empty());
 
-        //when & then
-        assertThrows(EntityNotFoundException.class, () -> subj.getLinkStats("non-existing link"));
+        EntityNotFoundException exception = assertThrows(EntityNotFoundException.class,
+                () -> ReflectionTestUtils.invokeMethod(linkService, "findLinkByShortUrl", shortUrl));
+        assertEquals("Requested resource '/l/nonexistent' not found", exception.getMessage());
     }
 }
